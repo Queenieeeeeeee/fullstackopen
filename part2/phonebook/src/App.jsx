6a91e6f014b56b3d8ce9bbec9bb582dcac13edde
Queenieeeeeeee <sty2460624@gmail.com>
2025-12-02 16:07:11 -0700

    Complete Part D

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import noteService from './services/persons.js'
 
   const Filter = ({ searchTerm, handleSearchTerm }) => {
     return (
@@ -24,11 +25,11 @@ import { useState } from 'react'
     )
   }
 
-  const Persons = ({filtershownames}) => {
+  const Persons = ({filtershownames, deletePerson}) => {
     return (
       <ul>
           {filtershownames.map(person => 
-          <li key={person.name}>{person.name} {person.number}</li>
+          <li key={person.name}>{person.name} {person.number} <button onClick={() => deletePerson(person.id)}>delete</button></li>
         )}
       </ul>
     )
@@ -36,16 +37,19 @@ import { useState } from 'react'
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchTerm, setSearchTerm] = useState('')
   
+  useEffect(() => {
+  noteService
+    .getAll()
+    .then(initialNotes => {
+      setPersons(initialNotes)
+    })
+}, [])
+
   const handleSearchTerm = (event) => {
     console.log(event.target.value)
     setSearchTerm(event.target.value)
@@ -64,21 +68,46 @@ const App = () => {
   const addNote = (event) => {
     event.preventDefault()
 
-    if (persons.find(person => person.name === newName)) {
-    alert(`${newName} is already added to phonebook`)
-    return}
-
-    const noteObject = {
-      name: newName,
-      number: newNumber
+    const exisitngPerson = persons.find(person => person.name === newName)
+    if (exisitngPerson) {
+      if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?` )) {
+        const updatedPerson = { ...exisitngPerson, number: newNumber }
+        noteService
+          .update(exisitngPerson.id, updatedPerson)
+          .then(returnedPerson => {
+            setPersons(persons.map(person => person.id === exisitngPerson.id ? returnedPerson : person))
+            setNewNumber('')
+          })
+      } 
     }
+      else {
+        const noteObject = {
+        name: newName,
+        number: newNumber
+      }
 
-  setPersons(persons.concat(noteObject))
-  setNewName('')
-  setNewNumber('')
+      noteService
+        .create(noteObject)
+        .then(returnedNote => {
+          setPersons(persons.concat(returnedNote))
+          setNewNumber('')
+          setNewName('')
+        })
+  }
 }
 
   const filtershownames = persons.filter(person => person.name.toLowerCase().includes(searchTerm.toLowerCase()))
+
+  const deletePerson = (id) => {
+    const person = persons.find(p => p.id === id)
+    if (window.confirm("Delete " + person.name + " ?")) {
+    noteService
+      .deletePerson(person.id) 
+      .then(() => {
+        setPersons(persons.filter(p => p.id !== person.id))
+      })
+  }
+}
 
   return (
     <div>
@@ -87,7 +116,7 @@ const App = () => {
       <h3>Add a new</h3>
       <PersonForm addNote={addNote} newName={newName} newNumber={newNumber} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} /> 
       <h2>Numbers</h2>
-      <Persons filtershownames={filtershownames} />
+      <Persons filtershownames={filtershownames} deletePerson={deletePerson} />
     </div>
   )
 }
--- part2/practice/src/App.jsx
+++ part2/practice/src/App.jsx
@@ -1,27 +1,62 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Note from './components/Note'
+import noteService from './services/notes'
+import Notification from './components/Notification'
+import Footer from './components/Footer'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('a new note...') 
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('some error happened...')
 
-  const handleNoteChange = (event) => {
-    console.log(event.target.value)
-    setNewNote(event.target.value)
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      })
+  }, [])
+
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    noteService
+      .update(id, changedNote).then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(error => {
+
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+        setNotes(notes.filter(n => n.id !== id))
+      })
   }
 
   const addNote = (event) => {
     event.preventDefault()
     const noteObject = {
       content: newNote,
-      important: Math.random() < 0.5,
-      id: String(notes.length + 1),
+      important: Math.random() > 0.5
     }
 
-  setNotes(notes.concat(noteObject))
-  setNewNote('')
-}
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
+  }
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
   const notesToShow = showAll
     ? notes
     : notes.filter(note => note.important === true)
@@ -29,6 +64,7 @@ const App = (props) => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
@@ -36,7 +72,11 @@ const App = (props) => {
       </div>      
       <ul>
         {notesToShow.map(note => 
-          <Note key={note.id} note={note} />
+          <Note
+            key={note.id}
+            note={note} 
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         )}
       </ul>
       <form onSubmit={addNote}>
@@ -45,6 +85,7 @@ const App = (props) => {
         />
         <button type="submit">save</button>
       </form>   
+      <Footer />
     </div>
   )
 }
